949cdb526e42c80659fa7b039b6f9f62ae86c608
davidalpha <david.alpha@gmail.com>
2021-01-15 23:38:25 +0100

    added trigger spawning per tile type with set amount

---
+++ Map.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var water_tile_array = []
+var trigger = preload("res://trigger.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	water_tile_array = get_cells_of_type($forestTileMap, 1)
+	seed_triggers(water_tile_array, 14)
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func get_cells_of_type(map, type):
+	var all = map.get_used_cells()
+	var cellsOfIndex = []
+	for v in all:
+		if map.get_cell(v.x, v.y) == type:
+			cellsOfIndex.append(v)
+	return cellsOfIndex
+
+func seed_triggers(tile_array, amount):
+	trigger = trigger.instance()
+	tile_array.shuffle()
+	for i in range(0,amount):
+		print(tile_array[i])
+		var pos_in_px = Vector2($forestTileMap.map_to_world(tile_array[i]))
+		print(pos_in_px)
+		trigger.position = pos_in_px
+		self.add_child(trigger)
+		
+		
+
